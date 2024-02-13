3b0f484d80263b46d7d9a56f48bc285d198137aa
Stephanyy <Stephanyy@DESKTOP-KVJF7PV>
2024-02-13 11:48:32 -0300

    Including Cors

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -18,6 +18,13 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("funcionariosApp", builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
